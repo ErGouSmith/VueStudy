8ace1296ef4b7558fe5c446851a33684ce65bd82
ErGouSmith <korg2009@hotmail.com>
2021-05-23 20:59:53 +0800

    finish assignment 1 and 2.

---
+++ Assignment/02-ShoppingCartDemo/main.js
@@ -0,0 +1,11 @@
+let app = new Vue({
+    el:"#app",
+    data:{
+        books:[
+            {id:1,goodsName:"《算法导论》",addCartTime:"2006-9",amount:2,price:75},
+            {id:2,goodsName:"《UNIX编程艺术》",addCartTime:"2006-9",amount:3,price:24},
+            {id:3,goodsName:"《编程珠玑》",addCartTime:"2006-9",amount:1,price:75},
+            {id:4,goodsName:"《代码大全》",addCartTime:"2006-9",amount:4,price:97}
+        ]
+    }
+})

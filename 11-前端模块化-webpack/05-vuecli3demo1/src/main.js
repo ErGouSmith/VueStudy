97dc3a587f608283c02c039b2f565dd4eaa8f9d2
ErGouSmith <korg2009@hotmail.com>
2021-06-09 17:55:35 +0800

    start vue router

--- 11-前端模块化-webpack/05-vuecli3demo1/src/main.js
+++ 11-前端模块化-webpack/05-vuecli3demo1/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 在npm run serve时是否显示提示信息
 Vue.config.productionTip = false
 
+// .$mount("#app") 相当于el:"#app", el最后在vue内部就是会进行mount挂载
 new Vue({
   render: h => h(App),
 }).$mount('#app')
